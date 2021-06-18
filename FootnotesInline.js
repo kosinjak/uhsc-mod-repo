043619ad51c028f671bd3f01d4c419968b247156
Gio <sethg@ipi.org>
2021-06-17 21:11:32 -0500

    oops symlink

--- FootnotesInline.js
+++ FootnotesInline.js
@@ -1,1 +1,44 @@
-../Asset Pack V1 - The  Unofficial Homestuck Collection/Asset Pack - The  Unofficial Homestuck Collection/mods/FootnotesInline.js
+
+var footnotes = {
+  "author": "Default author",
+  "story": {}
+}
+
+var test_pages = [
+    "001901",
+    "003503",
+    "003840",
+    "005664",
+    "005665",
+    "006009",
+    "007395",
+    "007614",
+    "007623",
+    "007687",
+    "007688", "007689",
+    "008143",
+    "008144",
+    "008282",
+    "008801",
+    "009987"
+]
+
+test_pages.forEach(num => {
+    footnotes.story[num] = [{
+      "content": "AutoFootnote <b>a2</b>",
+      "author": "username_a2",
+      "class": "css_a2"
+    },{
+      "content": "AutoPreface",
+      "author": "username_a2",
+      "class": "css_a2",
+      "preface": true
+    }]
+})
+
+module.exports = {
+    title: "Footnotes inline", 
+    desc: "test mod",
+    
+    footnotes: [footnotes]
+}
--- serial.js
+++ serial.js
@@ -1,1 +1,25 @@
-../Asset Pack V1 - The  Unofficial Homestuck Collection/Asset Pack - The  Unofficial Homestuck Collection/mods/serial.js
+module.exports = {
+    title: "Serial", 
+    desc: "Shows the time between pages when you hover over command links",
+
+    edit(archive) {
+
+    },
+    vueHooks: [{
+        matchName: "pageNav",
+        methods: {
+            commandText(nextpage, sup) {
+                this.$logger.debug("Method has binding")
+                let this_timestamp = this.DateTime.fromSeconds(Number(this.thisPage.timestamp))
+                let next_timestamp = this.DateTime.fromSeconds(Number(nextpage.timestamp))
+                
+                const duration = next_timestamp.diff(this_timestamp, ['months', 'days', 'hours', 'minutes', 'seconds']);
+                let relevant_times = Object.keys(duration.values).filter(s => duration.values[s])
+                
+                let durations = relevant_times.map(s => `${duration.values[s]} ${s}`)
+
+                return `<span title="${durations.join(', ')}">` + sup(nextpage) + "</span>"
+            },
+        },
+    }],
+}
