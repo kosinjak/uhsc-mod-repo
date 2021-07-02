656a849513c7c0a9c38117995d097bf58d22e0f2
Gio <sethg@ipi.org>
2021-07-01 22:50:15 -0500

    Companion Commentary footnotes

---
+++ HomestuckCompanionCommentary/make.py
@@ -0,0 +1,60 @@
+import json
+
+def vizToMspa(story_name, viz_num):
+    # Somehow this is actually correct
+    ret = viz_num
+
+    # assert int(viz_num) not in [
+    #     992,
+    #     4299,
+    #     4938,
+    #     4988,
+    #     9802,
+    #     9803,
+    #     9804
+    # ]
+
+    if story_name == "problem-sleuth":
+        ret = int(viz_num) + 218
+    elif story_name == "story":
+        ret = int(viz_num) + 1900
+
+    print(story_name, viz_num, ret, sep="\t")
+    return ret
+
+
+with open("commentary.json", "r", encoding="utf-8") as fp:
+    commentary = json.load(fp)
+
+andrew_footnotes = {
+    "author": "Andrew Hussie",
+    "story": {}
+}
+archivist_notes = {
+    "author": "/r/Homestuck archivists",
+    "story": {}
+}
+
+for record in commentary:
+    story_name = record['name']
+    for page in record['data']:
+        try:
+            pageid = (vizToMspa(story_name, page['id']))
+            pageid = f"{int(pageid):06}"
+
+            if pc := page.get("commentary"):
+                andrew_footnotes['story'][pageid] = [{"content": pc}]
+
+            if mn := page.get("notes"):
+                archivist_notes['story'][pageid] = [{"content": mn}]
+        except:
+            print(page)
+            raise
+
+footnoteCollection = [
+    andrew_footnotes,
+    archivist_notes
+]
+
+with open("footnotes.json", "w", encoding="utf-8") as fp:
+    json.dump(footnoteCollection, fp, ensure_ascii=False, indent=2)
---
+++ HomestuckCompanionCommentary/mod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    title: "Homestuck Companion Commentary", 
+    desc: "Transcription of the author commentary from the Homestuck books, with archivists' notes from the /r/Homestuck community.",
+    
+    footnotes: "./footnotes.json"
+}
